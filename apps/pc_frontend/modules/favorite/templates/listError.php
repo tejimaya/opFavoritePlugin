5a6022bbed51f8ee55c388edecca6f0915f764ab
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-24 18:27:20 +0000

    #3360:added "back to previous page" to the every *Error templates of the pc_* application
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opFavoritePlugin/trunk@10364 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/favorite/templates/listError.php
+++ apps/pc_frontend/modules/favorite/templates/listError.php
@@ -1,3 +1,4 @@
-<?php
+<?php include_box( 'notFavorite', __('Favorite'), __('There is not registration of the favorite yet.')); ?>
 
-include_box( 'notFavorite', __('Favorite'), __('There is not registration of the favorite yet.'));
+<?php use_helper('Javascript') ?>
+<p><?php echo link_to_function(__('前のページに戻る'), 'history.back()') ?></p>
