76fecbc39b1fc3dbeaf73d4a1d831411dca44fe3
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-26 06:04:51 +0000

    #3477:fixed changed op_include_parts and link
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opFavoritePlugin/trunk@10413 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/favorite/templates/_favoriteListBox.php
+++ apps/pc_frontend/modules/favorite/templates/_favoriteListBox.php
@@ -1,12 +1,12 @@
 <?php
-$option = array(
+$options = array(
   'title' => __('Favorite list'),
   'list' => $members,
   'link_to' => 'member/profile?id=',
-  'moreInfo' => array(sprintf('%s(%d)', __('Show all'), $cnt) => 'favorite/list'),
+  'moreInfo' => array(link_to(sprintf('%s(%d)', __('Show all'), $cnt), 'favorite/list')),
   'type' => 'full',
   'row' => $row,
   'col' => $col,
 );
 
-include_parts('nineTable', 'favoriteList', $option);
+op_include_parts('nineTable', 'favoriteList', $options);
