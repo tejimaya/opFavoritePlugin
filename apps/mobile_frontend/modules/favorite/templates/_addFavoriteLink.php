f0c716a500504236777374681d00dedd4411de27
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-11 03:10:05 +0900

    fixed duplication link in the mobile.

--- apps/mobile_frontend/modules/favorite/templates/_addFavoriteLink.php
+++ apps/mobile_frontend/modules/favorite/templates/_addFavoriteLink.php
@@ -2,7 +2,6 @@
 $form = new sfForm();
 if ($id !== $sf_user->getMemberId())
 {
-  echo link_to(__('Add favorite'), 'favorite/add?id=' . $id);
   echo link_to(__('Add favorite'), 'favorite/add?id='.$id.'&'.$form->getCSRFFieldName().'='.$form->getCSRFToken());
 }
 ?>
